8c9ea76254e15c435553954d243f3f31fda852c6
mgcg216 <36535459+mgcg216@users.noreply.github.com>
2018-03-12 21:45:48 -0700

    Add files via upload

---
+++ SoundDearlyBeloved.py
@@ -0,0 +1,47 @@
+"""I had alot of fun with win sounds lets mess with it more
+This code will be super messy but I just want to play with it more"""
+
+import winsound
+
+def Solfege():
+    winsound.Beep(523, 750)
+    winsound.Beep(523, 250)
+    winsound.Beep(392, 750)
+    winsound.Beep(392, 250)
+    winsound.Beep(349, 750)
+    winsound.Beep(349, 250)
+    winsound.Beep(587, 750)
+    winsound.Beep(587, 250)
+    winsound.Beep(523, 750)
+    winsound.Beep(523, 250)
+    winsound.Beep(392, 750)
+    winsound.Beep(392, 250)
+    winsound.Beep(349, 750)
+    winsound.Beep(349, 250)
+    winsound.Beep(587, 750)
+    winsound.Beep(587, 250)
+    winsound.Beep(622, 750)
+    winsound.Beep(622, 250)
+    winsound.Beep(587, 750)
+    winsound.Beep(587, 250)
+    winsound.Beep(783, 750)
+    winsound.Beep(783, 250)
+    winsound.Beep(698, 125)
+    winsound.Beep(783, 120)
+    winsound.Beep(698, 750)
+    winsound.Beep(698, 250)
+    winsound.Beep(622, 750)
+    winsound.Beep(622, 250)
+    winsound.Beep(587, 750)
+    winsound.Beep(587, 250)
+    winsound.Beep(523, 750)
+    winsound.Beep(523, 250)
+    winsound.Beep(466, 750)
+    winsound.Beep(466, 250)
+
+
+
+
+Solfege()
+Solfege()
+Solfege()
